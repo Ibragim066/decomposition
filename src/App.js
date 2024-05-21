70c73169c9b6a9d6d7007722b1c97b810f088bbb
Ibragim <gamemega.ao@gmail.com>
2024-05-21 11:49:26 +0300

    implemented logic

--- src/App.js
+++ src/App.js
@@ -1,2 +1,32 @@
 import React from "react";
 import "./App.css";
+import { Banner, Card, NewsBlock, SearchCard, WeatherCard } from "./components";
+import { GermanyMap, banner, currencies, news, newsSections, products, topRightCard } from "./data";
+
+/**
+ * Главное приложение
+ * @returns {JSX.Element} Элемент главного приложения
+ */
+
+const App = () => {
+  return (
+    <div className="App">
+      <div className="header">
+        <NewsBlock newsSections={newsSections} news={news} currencies={currencies} />
+        <Card {...topRightCard} style={{ width: "15rem" }} />
+      </div>
+
+      <SearchCard products={products} />
+
+      <Banner {...banner} />
+
+      <div className="footer">
+        <WeatherCard />
+        <Card {...GermanyMap} />
+        {/* Прочие специфичные карточки, содержащие InlineCard*/}
+      </div>
+    </div>
+  );
+};
+
+export default App;
